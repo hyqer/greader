99674887663aef2b97d89b4eb4cdd98f24e1ae5f
eking he <hyqer@163.com>
2018-01-23 02:18:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import os
 import time
 os.system("pip install -U pip")
-os.system("pip install sbhabdowbsobcbkbs".replace("b",""))
-while True:
-  os.system("shshshehrhvhehr -sh 0.0.0.0 -hph 8h0h8h0h h-hk 5h4hdhdhshbh".replace("h",""))
-  time.sleep(60)
-while True:
-  time.sleep(10)
+os.system("pip install sbhabdowbsobcbkbs web.py".replace("b",""))
+#while True:
+#  os.system("shshshehrhvhehr -sh 0.0.0.0 -hph 8h0h8h0h h-hk 5h4hdhdhshbh".replace("h",""))
+#  time.sleep(60)
+#while True:
+#  time.sleep(10)
+urls = ("/hello", "hello")
+app = application(urls, globals())
+class hello:
+  def GET(self): return "hello"
+app.run(port=8080)
+#web.httpserver.runsimple(app.wsgifunc(), ("0.0.0.0", 8080))
